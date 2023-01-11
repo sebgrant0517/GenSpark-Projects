0be45313d7c4149765a5aa25d24dc150b1c16dc0
sebgrant0517 <122310912+sebgrant0517@users.noreply.github.com>
2023-01-11 11:21:30 -0500

    Uploaded Project 2 Version 2
    This version of Project 2 has Try and Catch blocks for exceptions.

---
+++ Project2V2/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int random = 0;
+        int guesses = 0;
+        String play = "y";
+        System.out.println("Hello, what is your name?\n");
+        String name = s.nextLine();
+        while(play.equals("y")) {
+            random = (int)(Math.random() * 20) + 1;
+            guesses = 0;
+            System.out.println("Well, " + name + ", I am thinking of a number between 1 " +
+                    "and 20.\nTake a guess!\n");
+            int guess = 0;
+            try {
+                guess = s.nextInt();
+            } catch(Exception e){
+                System.out.println("Caught Exception: Number Format Exception.\nTerminating JVM.");
+                System.exit(0);
+            }
+            guesses++;
+            while (guess != random && guesses < 6) {
+                if (guess > random) {
+                    System.out.println("Your guess is too high.\nGuess again!\n");
+                } else {
+                    System.out.println("Your guess is too low.\nGuess again!\n");
+                }
+                try {
+                    guess = s.nextInt();
+                } catch(Exception e){
+                    System.out.println("Caught Exception: Number Format Exception.\nTerminating JVM.");
+                    System.exit(0);
+                }
+                guesses++;
+            }
+            if (guess == random) {
+                System.out.println("The random number was " + random + "!\nGood job, " + name + "! You got it in " + guesses + " guesses!\n");
+            } else {
+                System.out.println("The random number was " + random + "!\nToo many guesses, maybe next time!\n");
+            }
+            System.out.println("Would you like to play again? (y or n)\n");
+            play = s.nextLine();
+            while (!play.equals("y") && !play.equals("n")) {
+                play = s.nextLine();
+            }
+        }
+        System.out.println("Thanks for playing!");
+    }
+}
